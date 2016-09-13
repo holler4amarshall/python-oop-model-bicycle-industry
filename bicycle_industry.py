7e5ab97c1eb9043d2f7b313374b094dd619fbbd6
holly <holler4amarshall@hotmail.com>
2016-09-13 02:40:57 +0000

    adding a function to tally the running profit for shop

--- bicycle_industry.py
+++ bicycle_industry.py
@@ -15,8 +15,9 @@ class Customer:
 	    self.name = name
 	    self.budget = budget
 	    self.affordable_bicycles = []
-
-        
+	    self.purchases = []
+      
+       
 class Bicycle_store: 
     def __init__(self, name):
         '''Initialise Bicycle Stores with attributes for store name, 
@@ -25,10 +26,15 @@ class Bicycle_store:
         self.markup = 0
         self.retail_price = {}
         self.inventory = {}
+        self.profit = 0
         
     def get_markup(self):
         '''return the store markup'''
         return self.markup
+        
+    def get_bicycle_inventory(self, Bicycle):
+        print self.inventory[Bicycle]
+        return self.inventory[Bicycle]
     
     def set_retail_price(self, Bicycle):
         '''in a dictionary, note each bicycle and its retail price.
@@ -55,10 +61,32 @@ class Bicycle_store:
         else:
             print Customer.name + ', unfortunately we cannot offer any bikes within your budget.'
         
+        
     def current_inventory(self): 
         '''print and return the current inventory for the store'''
         current_inventory = ', '.join("{!s}={!r}".format(key,val) for (key,val) in self.inventory.items())
         print 'The current inventory at ' + self.name + ' is: ' + current_inventory
         return current_inventory
-		
-		
+        
+        
+    def customer_purchase(self, Customer, Bicycle):
+	    '''
+	    update the customer's list of purchases
+	    update the shop's inventory & sales records
+	    print the name of the bicycle purchased & 
+	    print the remaining value of money the customer has in their bicycle fund
+	    '''
+	    Customer.purchases.append(Bicycle.name)
+	    self.profit += Bicycle.cost
+	    self.inventory[Bicycle.name] -= 1 
+	    
+	    # THANKYOU MESSAGE:
+	    print 'Thankyou ' + Customer.name + ' for your purchase of the ' + Bicycle.name + \
+	    ' for $' + str(int(self.retail_price[Bicycle.name]/100)) + '. You have $' +\
+	    str(int(Customer.budget - (self.retail_price[Bicycle.name]/100)))\
+	    + ' remaining in your bicycle fund for your next purchase.'
+	    
+    def profit_statement(self):
+        '''print the running profit balance statement based on all sales'''
+        print 'The running profit balance is: $' + str(self.profit/100)
+            
--- main.py
+++ main.py
@@ -10,13 +10,14 @@ def main():
     ebike = Bicycle(name = "Faraday Porteur", weight = 4, cost = 25000)
     city = Bicycle(name = "Marin Fairfax SC6 DLX", weight = 7, cost = 17000)
     
-#instances of bicycles in a list
+#add instances of bicycles to a list
     bicycles = [ladies, urban, hipster, retro, ebike, city]
     
 #name store & add its margin & its inventory
     shop = Bicycle_store(name = "Holler4aBike!")
     shop.markup = 20
-    shop.inventory = {'ladies': 10, 'urban': 30, 'hipster': 5, 'retro': 5, 'ebike': 10, 'city': 30}
+    shop.inventory = {'Trek Chelsea 9': 10, 'Breezer Uptown': 30, 'Felt York': 5, 'Brooklyn Bicycle Co. Wythe': 5, 'Faraday Porteur': 10, 'Marin Fairfax SC6 DLX': 30}
+    #shop.inventory = {ladies: 10, urban: 30, hipster: 5, retro: 5, ebike: 10, city: 30}
     
 #set the retail price for the bicycles in the store        
     for bicycle in bicycles:
@@ -26,9 +27,10 @@ def main():
     holly = Customer(name = "Holly Marshall", budget = 200)
     moya = Customer(name = "Moya Vandeleur", budget = 500)
     caitlin = Customer(name = "Caitlin Cooper", budget = 1000)
+    lizzie = Customer(name = "Lizzie Slaughter", budget = 1500)
 
-#instances of customers in a list
-    customers = [holly, moya, caitlin]
+#add instances of customers to a list
+    customers = [holly, moya, caitlin, lizzie]
 
 #print customers and list of bikes that they can afford, given their budget
     for customer in customers: 
@@ -36,9 +38,20 @@ def main():
             shop.create_suggestion_list(bicycle, customer)
         shop.make_suggestions(customer)
     
-
 #print the shop's initial inventory
     shop.current_inventory()
-		
+    
+	
+#the customers will purchase a bicycle
+    shop.customer_purchase(moya, urban)
+    shop.customer_purchase(caitlin, ebike)
+    shop.customer_purchase(lizzie, retro)
+    
+#print the shop's remaining inventory and profit post sale
+    shop.profit_statement()
+    shop.current_inventory()
+ 
+    
+    
 if __name__ == "__main__": 
     main()
